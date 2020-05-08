b15f81ca29e182139e03ee1a30a08e5c14d3839e
Uriah Harston <uharston@gmail.com>
2020-05-08 14:42:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,10 +61,6 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-programmer_hash = {:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-   :languages => ["LISP", "C"]
-}}
 
 	programmer_hash =
  		{
@@ -81,6 +77,12 @@ programmer_hash = {:yukihiro_matsumoto => {
           :languages => ["C"]
         }
      }
+
+     programmer_hash = {:yukihiro_matsumoto => {
+       :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+     }}
+
 
 
 end
