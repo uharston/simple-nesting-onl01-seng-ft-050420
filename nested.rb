58a0d9d6fc393cd6b5037b2d6a999cdeea72a853
Uriah Harston <uharston@gmail.com>
2020-05-08 14:43:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,8 +103,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+    #  alans_new_info = "GUI"
 
 
 end
